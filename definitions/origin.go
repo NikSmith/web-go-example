11642dfc5ccb0cad695c83adcf4601590777f3a5
NikSmith <nikita@intelliq.ru>
2016-01-15 12:28:01 +0300

    Реализовано - Middleware

---
+++ definitions/origin.go
@@ -0,0 +1,24 @@
+package definitions
+import (
+	"webapi/webgo"
+)
+
+
+type Origin struct {
+	webgo.Middleware
+}
+
+func (h Origin) Handler (ctx *webgo.Context) bool {
+	origin := ctx.Request.Header.Get("Origin")
+	if origin == "" {
+		origin = "*"
+	}
+	ctx.Response.Header().Set("Access-Control-Allow-Headers", "accept, content-type");
+	ctx.Response.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	ctx.Response.Header().Set("Access-Control-Allow-Origin", origin);
+	ctx.Response.Header().Set("Access-Control-Allow-Credentials", "true");
+	ctx.Response.Header().Set("Access-Control-Expose-Headers", "Date");
+
+	return true
+}
+
--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 import (
 	"webapi/webgo"
 	"webapi/controllers"
+	"webapi/definitions"
 )
 
 
 func main()  {
-	webgo.Get("/",&controllers.Test{},nil,nil,"Index")
-	webgo.Get("/render",&controllers.Test{},nil,nil,"TestRender")
-	webgo.Get("/json",&controllers.Test{},nil,nil,"TestJson")
+	webgo.RegisterMiddleware("origin", definitions.Origin{})
+
+	webgo.Get("/",&controllers.Test{},"origin",nil,"Index")
+	webgo.Get("/render",&controllers.Test{},"",nil,"TestRender")
+	webgo.Get("/json",&controllers.Test{},"",nil,"TestJson")
 	webgo.Run(":8081")
 }
